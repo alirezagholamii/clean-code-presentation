e7e0d976ae34ceef64879e3cf1644985c5930104
Alireza <alireza.gholami403@gmail.com>
2021-07-06 19:10:27 +0430

    feat: add principles

---
+++ principles.js
@@ -0,0 +1,54 @@
+/*
+Three Principles for Clean Code
+
+1.The right tool for the job.
+    Boundaries Matter
+     🔅 HTML in JS string 🔴
+     🔅 JS in HTML 🔴
+
+     Stay Native - Advantage:
+     ✅ Cached
+     ✅ Code colored
+     ✅ Syntax checked
+     ✅ Separation of concerns
+     ✅ Reusable
+     ✅ Avoids string parsing
+     ✅ Can minify & obfuscate
+
+
+2.Maximize Signal to Noise Ratio
+    Signal:
+    logic that follows the TED rule:
+     ✅ Terse 
+     ✅ Expressive 
+     ✅ Do one thing 
+    
+     Noise:
+     🙉 Repetition
+     🙉 Huge classes
+     🙉 Unnecessary comments
+     🙉 Poorly named structures
+     🙉 Long methods
+     🙉 Overly verbose
+     🙉 No whitespace
+     🙉 Zombie code
+
+    🧠 Our brain is the compiler
+
+    🗿🗿🗿 Don't repeat yourself
+
+
+3.Self-documenting Code
+    Understanding the original programmer's intent is the most
+    difficult problem.
+
+    Well written code is self-documenting.
+    
+     ✅ Clear intent
+     ✅ Layers of abstractions
+     ✅ Format for readabilty
+     ✅ Favor code over comments
+
+
+
+*/
